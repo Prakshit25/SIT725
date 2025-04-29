c757d39ef870eccf33e8d9c7c40c0c9d3208b310
Prakshit25 <dobariyaprakshit@gmail.com>
2025-04-30 01:54:04 +1000

    Add files via upload

---
+++ Task 7.1P/Public/scripts.js
@@ -0,0 +1,9 @@
+
+// Connect to the server socket
+const socket = io();
+
+// Listen to 'number' event
+socket.on('number', (msg) => {
+  console.log('Random number:', msg);
+  document.getElementById('number').innerText = msg;
+});
